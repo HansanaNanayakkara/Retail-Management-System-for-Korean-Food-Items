1a4924f8ad2c6abbffa0f1a00bbed4dc35fe2bc9
Hansana Nanayakkara <157884826+HansanaNanayakkara@users.noreply.github.com>
2024-07-25 23:18:09 +0530

    Update insert.java

--- insert.java
+++ insert.java
@@ -14,10 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author sandunsrimal
- */
+
 public class insert extends javax.swing.JFrame {
 
     /**
